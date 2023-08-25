39e3b87fc4f5852e88da755b6440a0ff73724265
Franco Franzini <francofranzini12@gmail.com>
2023-08-25 09:33:15 -0300

    pasado index a source

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
-import axios from 'axios'
-
 const express = require('express')
 const app = express()
+const axios = require('axios')
+
 
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
 
-const hello = '<h1>Hello Nashe</h1'
+const fetchNotes = () =>{
+  axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    return response
+  })
+}
+
+const notes = fetchNotes()
+
+const hello = '<h1>Hello Nashe</h1>'
 
 app.get('/',(request, response) =>{
   response.send(hello)
